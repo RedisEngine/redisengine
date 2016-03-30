5f487831cce38232aa6effc7ed7c106cd88ef1e7
Joe Kavalieri <joe.kavalieri@gmail.com>
2016-03-30 23:22:13 +0200

    Initial commit

---
+++ redisengine/direct/__init__.py
@@ -0,0 +1,3 @@
+# from redisengine.direct.base.metaclasses import *
+# from redisengine.direct.operators import *
+# from redisengine.direct.tree import *
---
+++ redisengine/proxy/__init__.py
@@ -0,0 +1,4 @@
+from redisengine.proxy.operators import *
+from redisengine.proxy.tree import *
+
+__all__ = tree.__all__
